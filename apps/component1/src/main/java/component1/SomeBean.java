40dcfeb5edecfa441426039a62a8ab2ec748b681
Lars Hvile <lars@hulte.net>
2012-01-07 18:24:43 +0100

    Producing 'deployable' artifacts for all modules and publishing them to a flat-dir repo.

--- apps/component1/src/main/java/component1/SomeBean.java
+++ apps/component1/src/main/java/component1/SomeBean.java
@@ -1,15 +1,21 @@
 package component1;
 
 import javax.annotation.PostConstruct;
+
+import org.apache.commons.logging.*;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public final class SomeBean {
 
-    @PostConstruct
+    private final Log logger = LogFactory.getLog(getClass());
+
+
+    @PostConstruct // TODO not working in JBoss?? //         <module name="javax.annotation.api"/>????
     public void init() {
-        System.out.println("I'm alive");
+        logger.warn("\nI'm alive\n");
+        // throw new RuntimeException("kvakkel");
     }
 
 
